b702ae7f691ee58d93fad8f79f2f0fd69b06f316
unknown <willy.willian@hotmail.com>
2019-10-04 14:05:36 +0700

    Change Design

---
+++ header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Situs Judi Online - Bola88</title>
+        <link rel="icon" href="assets/icon/favicon.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="assets/icon/favicon.ico" type="image/x-icon" />
+        <link rel="apple-touch-icon" sizes="57x57" href="assets/icon/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="assets/icon/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="assets/icon/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="assets/icon/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="assets/icon/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="assets/icon/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="assets/icon/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="assets/icon/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="assets/icon/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192" href="assets/icon/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="assets/icon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="assets/icon/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="assets/icon/favicon-16x16.png">
+        <link rel="manifest" href="assets/icon/manifest.json">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="assets/icon/ms-icon-144x144.png">
+        <meta name="theme-color" content="#ffffff">
+        <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="assets/fontawesome/css/all.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Google+Sans" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">       
+        
+    </head>
+    <body>
+        <!-- div used to make the body blur -->
+        <div class="overlay" id="overlay" onclick="closeNav()"></div>
+        <section class="head index-part-1">
+            <div class="head-bar">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-6 col-xs-6 col-sm-6 col-md-6">
+                            <a href="/"><img class="img-responsive" src="assets/img/fw_logo.png"></a>
+                        </div>
+                        <div class="col-lg-6 col-xs-6 col-sm-6 col-md-6 text-right">
+                                
+                            <div id="mySidenav" class="sidenav text-center">
+                                <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a><br>
+                                <a href="/" class="fas fa-home"> Beranda</a>
+                                <hr>
+                                <a href="/kebijakan-privasi" class="fas fa-user-secret"> Kebijakan Privasi</a>         
+                                <a href="https://markasbola88.org/register" class="fas fa-user-plus" aria-hidden="true"> Register</a>                               
+                            </div>
+                            <div class="main" id="main">                                
+                                <a onclick="openNav()">MENU &#9776;</a>
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+                </div>
+            </div>            
+        </section>
---
+++ kebijakan-privasi.php
@@ -0,0 +1,112 @@
+<?php include 'header.php';?>
+<section class="generic-page">        
+    <div class="container">
+        <section class="generic text-center">
+            <h2>Kebijakan Privasi</h2>
+            <hr class="hr-mid">
+            <img alt="kebijakan-privasi" class="img-generic img-responsive" src="assets/img/kebijakan-privasi.jpg">
+        </section>
+        <section class="font-2">       
+
+            <p class="generic-p">Bola88 Mengoperasikan Situs Web https://bola88.netlify.com ("Layanan").</p>
+
+            <p class="generic-p">Halaman Ini Memberi Tahu Anda Tentang Kebijakan Kami Mengenai Pengumpulan, Penggunaan, Dan Pengungkapan Data Pribadi Saat Anda Menggunakan Layanan Kami Dan Pilihan Yang Telah Anda Kaitkan Dengan Data Itu. Kebijakan Privasi Kami Untuk Aktifparlay Dibuat Dengan Bantuan Generator Kebijakan Privasi Gratis.</p>
+
+            <p class="generic-p">Kami Menggunakan Data Anda Untuk Menyediakan Dan Meningkatkan Layanan. Dengan Menggunakan Layanan, Anda Menyetujui Pengumpulan Dan Penggunaan Informasi Sesuai Dengan Kebijakan Ini. Kecuali Ditentukan Lain Dalam Kebijakan Privasi Ini, Istilah Yang Digunakan Dalam Kebijakan Privasi Ini Memiliki Arti Yang Sama Seperti Dalam Syarat Dan Ketentuan Kami, Dapat Diakses Dari https://bola88.netlify.com</p>
+
+
+            <h2>Pengumpulan Dan Penggunaan Informasi</h2>
+
+            <p class="generic-p">Kami Mengumpulkan Beberapa Jenis Informasi Yang Berbeda Untuk Berbagai Keperluan Untuk Menyediakan Dan Meningkatkan Layanan Kami Kepada Anda.</p>
+
+            <h3>Jenis Data Yang Dikumpulkan</h3>
+
+            <h4>Data Pribadi</h4>
+
+            <p class="generic-p">Saat Menggunakan Layanan Kami, Kami Mungkin Meminta Anda Untuk Memberikan Kami Informasi Pengenal Pribadi Tertentu Yang Dapat Digunakan Untuk Menghubungi Atau Mengidentifikasi Anda ("Data Pribadi"). Informasi Yang Dapat Diidentifikasi Secara Pribadi Dapat Mencakup, Tetapi Tidak Terbatas Pada :</p>
+
+            <ul>
+                <li>Alamat Email</li>                
+                <li>Nomor Telepon</li>
+                <li>Cookie Dan Data Penggunaan</li>
+            </ul>
+
+            <h4>Data Penggunaan</h4>
+
+            <p class="generic-p">Kami Juga Dapat Mengumpulkan Informasi Bagaimana Layanan Diakses Dan Digunakan ("Data Penggunaan"). Data Penggunaan Ini Dapat Mencakup Informasi Seperti Alamat Protokol Internet Komputer Anda (Mis. Alamat IP), Jenis Browser, Versi Browser, Halaman Layanan Kami Yang Anda Kunjungi, Waktu Dan Tanggal Kunjungan Anda, Waktu Yang Dihabiskan Untuk Halaman-Halaman Itu, Unik Pengidentifikasi Perangkat Dan Data Diagnostik Lainnya.</p>
+
+            <h4>Pelacakan & Data Cookie</h4>
+            <p class="generic-p">Kami Menggunakan Cookie Dan Teknologi Pelacakan Serupa Untuk Melacak Aktivitas Di Layanan Kami Dan Menyimpan Informasi Tertentu.</p>
+            <p class="generic-p">Cookie Adalah File Dengan Sedikit Data Yang Dapat Menyertakan Pengidentifikasi Unik Anonim. Cookie Dikirim Ke Browser Anda Dari Situs Web Dan Disimpan Di Perangkat Anda. Teknologi Pelacakan Yang Juga Digunakan Adalah Suar, Tag, Dan Skrip Untuk Mengumpulkan Dan Melacak Informasi Dan Untuk Meningkatkan Dan Menganalisis Layanan Kami.</p>
+            <p class="generic-p">Anda Dapat Menginstruksikan Browser Anda Untuk Menolak Semua Cookie Atau Untuk Menunjukkan Kapan Cookie Dikirim. Namun, Jika Anda Tidak Menerima Cookie, Anda Mungkin Tidak Dapat Menggunakan Sebagian Layanan Kami.</p>
+            <p class="generic-p"><b>Contoh Cookie Yang Kami Gunakan:</b></p>
+            <ul>
+                <li><strong>Cookie Sesi.</strong> Kami Menggunakan Cookie Sesi Untuk Mengoperasikan Layanan Kami.</li>
+                <li><strong>Cookie Pilihan.</strong> Kami Menggunakan Cookie Preferensi Untuk Mengingat Preferensi Anda Dan Berbagai Pengaturan.</li>
+                <li><strong>Security Cookies.</strong> Kami Menggunakan Cookie Keamanan Untuk Tujuan Keamanan.</li>
+            </ul>
+
+            <h2>Penggunaan Data</h2>
+                
+            <p class="generic-p">Bola88 Menggunakan Data Yang Dikumpulkan Untuk Berbagai Keperluan :</p>    
+            <ul>
+                <li>Untuk Menyediakan Dan Memelihara Layanan</li>
+                <li>Untuk Memberitahukan Anda Tentang Perubahan Didalam Pelayanan Kami</li>
+                <li>Untuk Memungkinkan Anda Berpartisipasi Dalam Fitur Interaktif Layanan Kami Ketika Anda Memilih Untuk Melakukannya</li>
+                <li>Untuk Memberikan Layanan Dan Dukungan Pelanggan</li>
+                <li>Untuk Memberikan Analisis Atau Informasi Berharga Sehingga Kami Dapat Meningkatkan Layanan</li>
+                <li>Untuk Memantau Penggunaan Layanan</li>
+                <li>Untuk Mendeteksi, Mencegah Dan Mengatasi Masalah Teknis</li>
+            </ul>
+
+            <h2>Transfer Data</h2>
+            <p class="generic-p">Informasi Anda, Termasuk Data Pribadi, Dapat Ditransfer Ke - Dan Dipelihara Di - Komputer Yang Berlokasi Di Luar Negara Bagian, Provinsi, Negara Atau Yurisdiksi Pemerintah Lainnya Di Mana Undang-Undang Perlindungan Data Mungkin Berbeda Dari Yang Ada Di Yurisdiksi Anda.</p>
+            <p class="generic-p">Jika Anda Berada Di Luar Indonesia Dan Memilih Untuk Memberikan Informasi Kepada Kami, Harap Perhatikan Bahwa Kami Mentransfer Data, Termasuk Data Pribadi, Ke Indonesia Dan Memprosesnya Di Sana.</p>
+            <p class="generic-p">Persetujuan Anda Untuk Kebijakan Privasi Ini Diikuti Dengan Pengiriman Informasi Tersebut Merupakan Persetujuan Anda Untuk Transfer Tersebut.</p>
+            <p class="generic-p">Bola88 Akan Mengambil Semua Langkah Yang Wajar Diperlukan Untuk Memastikan Bahwa Data Anda Diperlakukan Dengan Aman Dan Sesuai Dengan Kebijakan Privasi Ini Dan Tidak Ada Transfer Data Pribadi Anda Yang Akan Terjadi Pada Suatu Organisasi Atau Negara Kecuali Jika Ada Kontrol Yang Memadai Di Tempat Termasuk Keamanan Anda Data Dan Informasi Pribadi Lainnya.</p>
+
+            <h2>Keterbukaan Data</h2>
+
+            <h3>Persyaratan Resmi</h3>
+            <p class="generic-p">Bola88 Dapat Mengungkapkan Data Pribadi Anda Dengan Itikad Baik Bahwa Tindakan Tersebut Diperlukan Untuk :</p>
+            <ul>
+                <li>Untuk Mematuhi Kewajiban Hukum</li>
+                <li>Untuk Melindungi Dan Mempertahankan Hak Atau Properti Bola88</li>
+                <li>Untuk Mencegah Atau Menyelidiki Kesalahan Yang Mungkin Terjadi Sehubungan Dengan Layanan</li>
+                <li>Untuk Melindungi Keamanan Pribadi Pengguna Layanan Atau Publik</li>
+                <li>Untuk Melindungi Terhadap Tanggung Jawab Hukum</li>
+            </ul>
+
+            <h2>Keamanan Data</h2>
+            <p class="generic-p">Keamanan Data Anda Penting Bagi Kami, Tetapi Ingat Bahwa Tidak Ada Metode Transmisi Melalui Internet, Atau Metode Penyimpanan Elektronik Yang 100% Aman. Meskipun Kami Berusaha Untuk Menggunakan Cara Yang Dapat Diterima Secara Komersial Untuk Melindungi Data Pribadi Anda, Kami Tidak Dapat Menjamin Keamanan Mutlaknya.</p>
+
+            <h2>Penyedia Jasa</h2>
+            <p class="generic-p">Kami Dapat Mempekerjakan Perusahaan Pihak Ketiga Dan Individu Untuk Memfasilitasi Layanan Kami ("Penyedia Layanan"), Untuk Memberikan Layanan Atas Nama Kami, Untuk Melakukan Layanan Terkait Layanan Atau Untuk Membantu Kami Dalam Menganalisis Bagaimana Layanan Kami Digunakan.</p>
+            <p class="generic-p">Pihak Ketiga Ini Memiliki Akses Ke Data Pribadi Anda Hanya Untuk Melakukan Tugas-Tugas Ini Atas Nama Kami Dan Berkewajiban Untuk Tidak Mengungkapkan Atau Menggunakannya Untuk Tujuan Lain Apa Pun.</p>
+
+            <h2>Tautan Ke Situs Lain</h2>
+            <p class="generic-p">Layanan Kami Dapat Berisi Tautan Ke Situs Lain Yang Tidak Dioperasikan Oleh Kami. Jika Anda Mengklik Tautan Pihak Ketiga, Anda Akan Diarahkan Ke Situs Pihak Ketiga Itu. Kami Sangat Menyarankan Anda Untuk Meninjau Kebijakan Privasi Setiap Situs Yang Anda Kunjungi.</p>
+            <p class="generic-p">Kami Tidak Memiliki Kendali Atas Dan Tidak Bertanggung Jawab Atas Konten, Kebijakan Privasi, Atau Praktik Situs Atau Layanan Pihak Ketiga Mana Pun.</p>
+
+
+            <h2>Privasi Anak-Anak</h2>
+            <p class="generic-p">Layanan Kami Tidak Membahas Siapa Pun Yang Berusia Di Bawah 18 ("Anak-Anak").</p>
+            <p class="generic-p">Kami Tidak Secara Sadar Mengumpulkan Informasi Yang Dapat Diidentifikasi Secara Pribadi Dari Siapa Pun Yang Berusia Di Bawah 18 Tahun. Jika Anda Adalah Orang Tua Atau Wali Dan Anda Mengetahui Bahwa Anak-Anak Anda Telah Memberikan Kepada Kami Data Pribadi, Silakan Hubungi Kami. Jika Kami Mengetahui Bahwa Kami Telah Mengumpulkan Data Pribadi Dari Anak-Anak Tanpa Verifikasi Izin Orang Tua, Kami Mengambil Langkah-Langkah Untuk Menghapus Informasi Itu Dari Server Kami.</p>
+
+
+            <h2>Perubahan Terhadap Kebijakan Privasi Ini</h2>
+            <p class="generic-p">Kami Dapat Memperbarui Kebijakan Privasi Kami Dari Waktu Ke Waktu. Kami Akan Memberi Tahu Anda Tentang Segala Perubahan Dengan Memposting Kebijakan Privasi Baru Di Halaman Ini.</p>
+            <p class="generic-p">Kami Akan Memberi Tahu Anda Melalui Email Dan / Atau Pemberitahuan Penting Pada Layanan Kami, Sebelum Perubahan Menjadi Efektif Dan Memperbarui "Tanggal Efektif" Di Bagian Atas Kebijakan Privasi Ini.</p>
+            <p class="generic-p">Anda Disarankan Untuk Meninjau Kebijakan Privasi Ini Secara Berkala Untuk Setiap Perubahan. Perubahan Pada Kebijakan Privasi Ini Efektif Ketika Diposkan Pada Halaman Ini..</p>
+
+
+            <h2>Hubungi Kami</h2>
+            <p class="generic-p">Jika Anda Memiliki Pertanyaan Tentang Kebijakan Privasi Ini, Silakan Hubungi Kami::</p>
+            <ul>
+                <li>Melalui Email : markasbola88@gmail.com</li>          
+            </ul>
+                 
+        </section>
+    </div>
+</section>
+<?php include 'footer.php';?>
